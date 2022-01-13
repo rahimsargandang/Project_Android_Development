232995ad8b21e61f7a01d4d2008d8f5998e7bc05
rahimsargandang <a178804@siswa.ukm.edu.my>
2022-01-14 02:53:43 +0800

    guide

--- app/src/main/java/com/example/twiceapps/guide_depression.java
+++ app/src/main/java/com/example/twiceapps/guide_depression.java
@@ -3,6 +3,8 @@ package com.example.twiceapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class guide_depression extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class guide_depression extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_depression);
+        VideoView videoView = findViewById(R.id.vid_stress);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.depression);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
     }
 }
--- app/src/main/java/com/example/twiceapps/guide_stress.java
+++ app/src/main/java/com/example/twiceapps/guide_stress.java
@@ -3,6 +3,8 @@ package com.example.twiceapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class guide_stress extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class guide_stress extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_stress);
+        VideoView videoView = findViewById(R.id.vid_stress);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.stress);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
     }
 }
